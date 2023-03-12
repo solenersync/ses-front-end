ada6eba5c37a71dcfd9d59f786f7aba39d094e63
Brian Kinsella <>
2023-03-12 09:59:19 +0000

    added more test coverage

---
+++ __tests__/Dashboard.snap.tsx
@@ -0,0 +1,78 @@
+import { getSolarForecast } from "api/solarForecastApi";
+import { getUser } from "api/userApi";
+import client, { User, Session } from "next-auth";
+import { getArrayData } from "../api/solarArrayApi";
+import { ISolarArray } from "../types/ISolarArray";
+import { Irradiance } from "../types/Irradiance";
+import { SessionProvider, useSession as originalUseSession } from "next-auth/react";
+import { render } from '@testing-library/react';
+import Dashboard from 'pages/dashboard';
+import Router from "next/router";
+
+jest.mock("api/userApi");
+jest.mock("api/solarArrayApi");
+jest.mock("api/solarForecastApi");
+jest.mock("next-auth/react", () => ({
+  ...jest.requireActual("next-auth/react"),
+  useSession: jest.fn(),
+}));
+jest.mock("next/router", () => ({
+  push: jest.fn(),
+  replace: jest.fn(),
+  prefetch: jest.fn(),
+}));
+
+describe("Dashboard", () => {
+  const user: User = {
+    name: "John Doe",
+    email: "jd@test.com",
+    userId: "1",
+    id: "",
+  };
+  const solarArray: ISolarArray = {
+    solarArrayId: 1,
+    lon: 1,
+    lat: 1,
+    peakPower: 1,
+    systemLoss: 1,
+    angle: 1,
+    aspect: 1,
+    mounting: "test",
+    userId: "1",
+  };
+  const forecast: Irradiance = {
+    time: "2023-02-24T00:00:00",
+    month: 1,
+    peakGlobalOutput: 1,
+    "G(i)": 1,
+    "Gb(i)": 1,
+    "Gd(i)": 1,
+    "Gcs(i)": 1,
+  };
+  const sessionData = { status: "unauthenticated", data: null };
+  const session = { user: { name: 'John Doe', email: "", userId: "", id:"" }, expires: "" };
+  const mockUseSession = originalUseSession as jest.Mock;
+
+  beforeEach(() => {
+    (getUser as jest.Mock).mockResolvedValue(user);
+    (getArrayData as jest.Mock).mockResolvedValue(solarArray);
+    (getSolarForecast as jest.Mock).mockResolvedValue(forecast);
+    mockUseSession.mockReturnValue({
+      status: "unauthenticated",
+      data: null,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render the dashboard", async () => {
+    const { container } = render(
+      <SessionProvider session={session}>
+        <Dashboard />
+      </SessionProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
--- types/Irradiance.tsx
+++ types/Irradiance.tsx
@@ -1,6 +1,7 @@
 export interface Irradiance {
   time: string;
   month: number;
+  peakGlobalOutput: number;
   'G(i)': number;
   'Gb(i)': number;
   'Gd(i)': number;
