52e042474f391641c37444b5b0fc4fb0a7bf2a44
Brian Kinsella <>
2023-03-15 14:46:30 +0000

    renaming

--- __tests__/AddArrayCard.snap.tsx
+++ __tests__/AddArrayCard.snap.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import AddArrayCard from 'components/AddArrayCard';
+import AddArrayCard from 'components/addArrayCard';
 
 describe('AddArrayCard', () => {
   it('should render the AddArrayCard', () => {
