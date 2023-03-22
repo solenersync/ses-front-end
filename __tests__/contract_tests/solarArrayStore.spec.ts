6de5b11543b5ee5a081c7b89307bed5010d619a3
Brian Kinsella <>
2023-03-22 21:10:56 +0000

    added more tests

--- __tests__/contract_tests/solarArrayStore.spec.ts
+++ __tests__/contract_tests/solarArrayStore.spec.ts
@@ -18,11 +18,12 @@ describe('solar-array-store contract tests', () => {
   const user: User = { name: 'John Doe', email: 'jd@test.com',  userId: 1, id:'' };
   const solarArray: ISolarArray = { solarArrayId:1, userId: 1, lat: 52.207306, lon: -6.52026, peakPower: 8.2, loss: 0.145, angle: 35.0, aspect: 2.0, mounting: "FREE"};
   const solarArrayPayload: ICreateSolarArray = { userId: 1, lat: 52.207306, lon: -6.52026, peakPower: 8.2, loss: 0.145, angle: 35.0, aspect: 2.0, mounting: "FREE"};
+  const invalidSolarArrayPayload: any = { userId: 1, lon: -6.52026, peakPower: 8.2, loss: 0.145, angle: 35.0, aspect: 2.0, mounting: "FREE"};
 
-  test('get user solar array', async () => {
+  test('a solar array exists', async () => {
 
     provider.addInteraction({
-      states: [{description: 'should return a solar array'}],
+      states: [{description: 'a solar array exists'}],
       uponReceiving: 'a request to get a solar array',
       withRequest: {
         method: 'GET',
@@ -47,11 +48,33 @@ describe('solar-array-store contract tests', () => {
       });
     
   });
-  test('create solar array', async () => {
+
+  test('a solar array doesnt exist', async () => {
 
     provider.addInteraction({
-      states: [{description: 'should create solar array and return array details'}],
-      uponReceiving: 'a valid payload for create solar array',
+      states: [{description: 'a solar array doesnt exist'}],
+      uponReceiving: 'a request to get a solar array',
+      withRequest: {
+        method: 'GET',
+        path: '/api/v1/solar-arrays/array/user/1',
+      },
+      willRespondWith: {
+        status: 404,
+      },
+    });
+
+    await provider.executeTest(async (mockService) => {
+      process.env.API_BASE_URL = mockService.url;
+      const resp = await getArrayData(user.userId);
+      expect(resp).toBeNull();
+      });
+    
+  });
+  test('a solar array is created', async () => {
+
+    provider.addInteraction({
+      states: [{description: 'a solar array is created'}],
+      uponReceiving: 'a valid request to create a solar array',
       withRequest: {
         method: 'POST',
         path: '/api/v1/solar-arrays/create',
@@ -79,10 +102,41 @@ describe('solar-array-store contract tests', () => {
     
   });
 
-  test('update solar array', async () => {
+  test('a solar array fails to be created', async () => {
 
     provider.addInteraction({
-      states: [{description: 'should update solar array and return status 200'}],
+      states: [{description: 'a solar array fails to be created'}],
+      uponReceiving: 'an invalid payload for create solar array',
+      withRequest: {
+        method: 'POST',
+        path: '/api/v1/solar-arrays/create',
+        body: {
+          userId: invalidSolarArrayPayload.userId,
+          lon: invalidSolarArrayPayload.lon,
+          peakPower: invalidSolarArrayPayload.peakPower,
+          loss: invalidSolarArrayPayload.loss,
+          angle: invalidSolarArrayPayload.angle,
+          aspect: invalidSolarArrayPayload.aspect,
+          mounting: invalidSolarArrayPayload.mounting,
+        },
+      },
+      willRespondWith: {
+        status: 500,
+      },
+    });
+
+    await provider.executeTest(async (mockService) => {
+      process.env.API_BASE_URL = mockService.url;
+      const resp = await createArray(invalidSolarArrayPayload);
+      expect(resp).toBeNull();
+      });
+    
+  });
+
+  test('a solar array is updated', async () => {
+
+    provider.addInteraction({
+      states: [{description: 'a solar array is updated'}],
       uponReceiving: 'a valid payload for update solar array',
       withRequest: {
         method: 'POST',
@@ -108,6 +162,38 @@ describe('solar-array-store contract tests', () => {
       process.env.API_BASE_URL = mockService.url;
       const resp = await updateArray(solarArray);
       expect(resp.status).toEqual(200);
+    });
+    
+  });
+
+  test('a solar array fails to update', async () => {
+
+    provider.addInteraction({
+      states: [{description: 'a solar array fails to update'}],
+      uponReceiving: 'an invalid payload for update solar array',
+      withRequest: {
+        method: 'POST',
+        path: '/api/v1/solar-arrays/update',
+        body: {
+          solarArrayId: invalidSolarArrayPayload.solarArrayId,
+          userId: invalidSolarArrayPayload.userId,
+          lon: invalidSolarArrayPayload.lon,
+          peakPower: invalidSolarArrayPayload.peakPower,
+          loss: invalidSolarArrayPayload.loss,
+          angle: invalidSolarArrayPayload.angle,
+          aspect: invalidSolarArrayPayload.aspect,
+          mounting: invalidSolarArrayPayload.mounting,
+        },
+      },
+      willRespondWith: {
+        status: 500
+      },
+    });
+
+    await provider.executeTest(async (mockService) => {
+      process.env.API_BASE_URL = mockService.url;
+      const resp = await updateArray(invalidSolarArrayPayload);
+      expect(resp).toBeNull();
     });
     
   });
