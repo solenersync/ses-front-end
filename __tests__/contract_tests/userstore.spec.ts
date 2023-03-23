59582dc1b72191bd7bba69cecca87dba090e24eb
Brian Kinsella <>
2023-03-23 09:17:49 +0000

    to test

--- __tests__/contract_tests/userstore.spec.ts
+++ __tests__/contract_tests/userstore.spec.ts
@@ -32,7 +32,7 @@ describe('userstore contract tests', () => {
         body: {email: user.email},
       },
       willRespondWith: {
-        status: 201,
+        status: 301,
         body: like({
           email: userResp.email,
           name: userResp.name,
@@ -44,7 +44,7 @@ describe('userstore contract tests', () => {
     await provider.executeTest(async (mockService) => {
       process.env.API_BASE_URL = mockService.url;
       const resp = await getUser(user.email);
-      expect(resp.status).toEqual(201);
+      expect(resp.status).toEqual(301);
       expect(resp.data.email).toEqual(userResp.email);
       expect(resp.data.name).toEqual(userResp.name);
       expect(resp.data.userId).toEqual(userResp.userId);
