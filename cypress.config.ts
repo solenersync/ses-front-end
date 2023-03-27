e4cd7e5bab24d0de8779989ceb480f8419fa6401
Brian Kinsella <>
2023-03-27 21:23:35 +0100

    try domain

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD,
     TEST_USER_NAME: process.env.TEST_USER_NAME,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://www.solenersync.net',
     codeCoverage: {
       url: '/api/__coverage__',
     },
