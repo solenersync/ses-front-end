6ea65994d84bb9412ecf12a40043f6d53f2b1b57
Brian Kinsella <>
2023-03-27 21:12:01 +0100

    try auth env

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     TEST_USER_EMAIL: process.env.TEST_USER_EMAIL,
     TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD,
     TEST_USER_NAME: process.env.TEST_USER_NAME,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     baseUrl: 'http://localhost:3000',
     codeCoverage: {
       url: '/api/__coverage__',
