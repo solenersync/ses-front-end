d1ba0d2681580d39d551e9ea303cc1339dc186c3
Brian Kinsella <>
2023-03-28 08:49:01 +0100

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,15 +5,15 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: 'https://www.solenersync.net',
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://www.solenersync.net',
+    // baseUrl: 'http://localhost:3000',
   },
   env: {
     TEST_USER_EMAIL: process.env.TEST_USER_EMAIL,
     TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD,
     TEST_USER_NAME: process.env.TEST_USER_NAME,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://www.solenersync.net',
     codeCoverage: {
       url: '/api/__coverage__',
     },
